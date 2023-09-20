8e83bd00c8eca5de254c07e1dd444053db256324
Ian Blake <ian@ianbl8.com>
2023-09-20 17:45:41 +0100

    Initial event handling for add button

--- src/main/java/org/wit/placemark/PlacemarkActivity.kt
+++ src/main/java/org/wit/placemark/PlacemarkActivity.kt
@@ -2,15 +2,34 @@ package org.wit.placemark
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import org.wit.placemark.databinding.ActivityPlacemarkBinding
 import timber.log.Timber
 import timber.log.Timber.Forest.i
 
 class PlacemarkActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPlacemarkBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_placemark)
 
+        binding = ActivityPlacemarkBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         Timber.plant(Timber.DebugTree())
         i("Placemark activity started")
+
+        binding.btnAdd.setOnClickListener() {
+            val placemarkTitle = binding.placemarkTitle.text.toString()
+            if (placemarkTitle.isNotEmpty()) {
+                i("Add button pressed: $placemarkTitle")
+            } else {
+                Snackbar
+                    .make(it, "Please enter a title", Snackbar.LENGTH_LONG)
+                    .show()
+            }
+        }
     }
 }
