591b8200524d40f3bb2336972734a637f9cf476f
Ian Blake <ian@ianbl8.com>
2023-09-20 17:56:16 +0100

    Refactor to use data model

--- src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
+++ src/main/java/org/wit/placemark/activities/PlacemarkActivity.kt
@@ -1,20 +1,20 @@
-package org.wit.placemark
+package org.wit.placemark.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import org.wit.placemark.databinding.ActivityPlacemarkBinding
+import org.wit.placemark.models.PlacemarkModel
 import timber.log.Timber
 import timber.log.Timber.Forest.i
 
 class PlacemarkActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPlacemarkBinding
+    var placemark = PlacemarkModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_placemark)
-
         binding = ActivityPlacemarkBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -22,14 +22,14 @@ class PlacemarkActivity : AppCompatActivity() {
         i("Placemark activity started")
 
         binding.btnAdd.setOnClickListener() {
-            val placemarkTitle = binding.placemarkTitle.text.toString()
-            if (placemarkTitle.isNotEmpty()) {
-                i("Add button pressed: $placemarkTitle")
+            placemark.title = binding.placemarkTitle.text.toString()
+            if (placemark.title.isNotEmpty()) {
+                i("Add button pressed: $placemark.title")
             } else {
                 Snackbar
                     .make(it, "Please enter a title", Snackbar.LENGTH_LONG)
                     .show()
             }
-        }
+       }
     }
 }
