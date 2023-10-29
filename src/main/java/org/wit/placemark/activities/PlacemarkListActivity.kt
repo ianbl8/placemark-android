7f1aa1f9084d5b688e6c114628525e2e60491b02
Ian Blake <ian@ianbl8.com>
2023-10-29 14:29:29 +0000

    Move PlacemarkAdapter to its own adapters class

--- src/main/java/org/wit/placemark/activities/PlacemarkListActivity.kt
+++ src/main/java/org/wit/placemark/activities/PlacemarkListActivity.kt
@@ -4,18 +4,14 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
-import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import org.wit.placemark.R
+import org.wit.placemark.adapters.PlacemarkAdapter
 import org.wit.placemark.databinding.ActivityPlacemarkListBinding
-import org.wit.placemark.databinding.CardPlacemarkBinding
 import org.wit.placemark.main.MainApp
-import org.wit.placemark.models.PlacemarkModel
 
 class PlacemarkListActivity : AppCompatActivity() {
 
@@ -63,30 +59,3 @@ class PlacemarkListActivity : AppCompatActivity() {
 
 }
 
-class PlacemarkAdapter constructor(private var placemarks: List<PlacemarkModel>) :
-    RecyclerView.Adapter<PlacemarkAdapter.MainHolder>() {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
-        val binding = CardPlacemarkBinding
-            .inflate(LayoutInflater.from(parent.context), parent, false)
-        return MainHolder(binding)
-    }
-
-    override fun onBindViewHolder(holder: MainHolder, position: Int) {
-        val placemark = placemarks[holder.adapterPosition]
-        holder.bind(placemark)
-    }
-
-    override fun getItemCount(): Int = placemarks.size
-
-    class MainHolder(private val binding: CardPlacemarkBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-
-        fun bind(placemark: PlacemarkModel) {
-            binding.placemarkTitle.text = placemark.title
-            binding.placemarkDescription.text = placemark.description
-        }
-
-    }
-
-}
