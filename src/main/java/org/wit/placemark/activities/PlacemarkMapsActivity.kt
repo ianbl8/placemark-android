5512cb921304d5e3ea9f938833a4985477464e19
Ian Blake <ian@ianbl8.com>
2023-12-12 17:58:33 +0000

    Introduce map view for all placemarks

--- src/main/java/org/wit/placemark/activities/PlacemarkMapsActivity.kt
+++ src/main/java/org/wit/placemark/activities/PlacemarkMapsActivity.kt
@@ -2,19 +2,25 @@ package org.wit.placemark.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
 import org.wit.placemark.databinding.ActivityPlacemarkMapsBinding
+import org.wit.placemark.databinding.ContentPlacemarkMapsBinding
 
 class PlacemarkMapsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPlacemarkMapsBinding
+    private lateinit var contentBinding: ContentPlacemarkMapsBinding
+    lateinit var map: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityPlacemarkMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
+
+        contentBinding = ContentPlacemarkMapsBinding.bind(binding.root)
+        contentBinding.mapView.onCreate(savedInstanceState)
 
     }
 
