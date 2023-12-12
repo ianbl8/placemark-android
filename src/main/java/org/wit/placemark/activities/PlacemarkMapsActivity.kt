29fcec74f4b9c6a88e9fb43570029b8e24017541
Ian Blake <ian@ianbl8.com>
2023-12-12 17:47:39 +0000

    Simplify PlacemarkMapsActivity

--- src/main/java/org/wit/placemark/activities/PlacemarkMapsActivity.kt
+++ src/main/java/org/wit/placemark/activities/PlacemarkMapsActivity.kt
@@ -1,18 +1,11 @@
 package org.wit.placemark.activities
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import org.wit.placemark.R
 import org.wit.placemark.databinding.ActivityPlacemarkMapsBinding
 
 class PlacemarkMapsActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityPlacemarkMapsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,19 +16,6 @@ class PlacemarkMapsActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_placemark_maps)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_placemark_maps)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
-    }
 }
