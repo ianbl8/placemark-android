9f886a02ba3f8bfbe75085765e6fc2085021eb7b
Ian Blake <ian@ianbl8.com>
2023-12-13 22:20:10 +0000

    Update MapView databinding to match xml filenames

--- src/main/java/org/wit/placemark/views/map/MapView.kt
+++ src/main/java/org/wit/placemark/views/map/MapView.kt
@@ -5,28 +5,28 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.Marker
 import com.squareup.picasso.Picasso
-import org.wit.placemark.databinding.ActivityPlacemarkMapsBinding
-import org.wit.placemark.databinding.ContentPlacemarkMapsBinding
+import org.wit.placemark.databinding.ActivityMapBinding
+import org.wit.placemark.databinding.ContentMapBinding
 import org.wit.placemark.main.MainApp
 import org.wit.placemark.models.PlacemarkModel
 
 class MapView : AppCompatActivity(), GoogleMap.OnMarkerClickListener {
 
-    private lateinit var binding: ActivityPlacemarkMapsBinding
-    private lateinit var contentBinding: ContentPlacemarkMapsBinding
+    private lateinit var binding: ActivityMapBinding
+    private lateinit var contentBinding: ContentMapBinding
     lateinit var app: MainApp
     lateinit var presenter: MapPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         app = application as MainApp
-        binding = ActivityPlacemarkMapsBinding.inflate(layoutInflater)
+        binding = ActivityMapBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
 
         presenter = MapPresenter(this)
 
-        contentBinding = ContentPlacemarkMapsBinding.bind(binding.root)
+        contentBinding = ContentMapBinding.bind(binding.root)
         contentBinding.mapView.onCreate(savedInstanceState)
         contentBinding.mapView.getMapAsync {
             presenter.doPopuplateMap(it)
